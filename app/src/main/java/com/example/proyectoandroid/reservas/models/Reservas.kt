3c9f5c5f5fd58824e8ca930402bd78742d4f85ea
ada99tf <69634645+ada99tf@users.noreply.github.com>
2023-02-22 19:18:37 +0100

    se ha actualizado la forma de la interfaz de las reservas

---
+++ app/src/main/java/com/example/proyectoandroid/reservas/models/Reservas.kt
@@ -0,0 +1,9 @@
+package com.example.proyectoandroid.reservas.models
+
+data class Reservas(
+    val fecha :  String,
+    val profesor : String,
+    val grupo : String,
+    val aula : String,
+    val hora : String
+)
