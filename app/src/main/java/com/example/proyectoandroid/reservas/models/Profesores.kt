a1ba4133c353b9fa204bbd600a540b3151e592eb
ada99tf <69634645+ada99tf@users.noreply.github.com>
2023-02-25 12:48:09 +0100

    Añadidos diversos archivos txt...

--- app/src/main/java/com/example/proyectoandroid/reservas/models/Profesores.kt
+++ app/src/main/java/com/example/proyectoandroid/reservas/models/Profesores.kt
@@ -3,7 +3,6 @@ package com.example.proyectoandroid.reservas.models
 data class Profesores(
 
     val nombre : String,
-    val grupos : List<grupos>,
     val rol : String,
     val usuario : String,
     val clave : String
