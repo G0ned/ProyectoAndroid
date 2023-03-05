eb09e6f6eb73ad181e6d832b330640ce5e164b43
ada99tf <69634645+ada99tf@users.noreply.github.com>
2023-03-05 15:16:20 +0100

    se ha arreglado el bugg del spinner

--- app/src/main/java/com/example/proyectoandroid/main/MainActivity.kt
+++ app/src/main/java/com/example/proyectoandroid/main/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.proyectoandroid.main
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.findNavController
 import com.example.proyectoandroid.R
 
 // activity principal en la cuál estan montado todos los fragmentos.
@@ -11,4 +15,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/proyectoandroid/reservas/views/MisReservasFragment.kt
+++ app/src/main/java/com/example/proyectoandroid/reservas/views/MisReservasFragment.kt
@@ -1,14 +1,18 @@
 package com.example.proyectoandroid.reservas.views
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -95,11 +99,35 @@ class MisReservasFragment : Fragment() {
     // función para ver toda la información en otro fragmento.
     private  fun EliminarReserva(reserva : Reservas) {
 
-        ReservasProv.removeReservas(reserva)
-        Toast.makeText(context, "Reserva eliminada" , Toast.LENGTH_SHORT).show()
-        findNavController().navigate(R.id.misReservasFragment, args =  arguments)
+
+        val botonOK =  { dialog : DialogInterface, witch : Int ->
+
+            ReservasProv.removeReservas(reserva)
+            Toast.makeText(context, "Reserva eliminada" , Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.misReservasFragment, args =  arguments)
+
+        }
+
+        val botonNoOK =  { dialog : DialogInterface, witch : Int ->
+
+            dialog.dismiss()
+
+        }
+
+
+            val dialogoCerrarAPP = AlertDialog.Builder(requireContext())
+            dialogoCerrarAPP.setMessage("¿Seguro que desea eliminar la reserva?")
+            dialogoCerrarAPP.setPositiveButton("Confirmar", DialogInterface.OnClickListener(botonOK))
+            dialogoCerrarAPP.setNegativeButton("Cancelar", DialogInterface.OnClickListener(botonNoOK))
+            dialogoCerrarAPP.show()
+
 
     }
+
+
+
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
